fc5f38d58c372eead9d89ef5a6db30b1b48602c8
Pritish Prasant Sahoo <pps491415@gmail.com>
2025-03-23 15:17:16 +0530

    Fixed the flex containers

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
         showNavigationContainer &&
         <NavigationContainer/>
       }
+      {/* <NavigationContainer/> */}
       <DeckContainer deckName=""/>
       {/* <div>Some more text</div> */}
       {/* <FeedContainer/> */}
